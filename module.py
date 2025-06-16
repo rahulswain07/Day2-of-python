b07e4c5261042e8d33f2c649e91937bfb454a7a2
Rahul swain <rahulswain18182112@gmail.com>
2025-06-16 05:35:44 +0530

    Update module.py

--- module.py
+++ module.py
@@ -1,8 +1,6 @@
-#module and comment
+#module 
 import pyjokes
-#print('printing jokes')
 #this print a random joke
 joke=pyjokes.get_joke()
 print(joke)
-"""this is amazing an
-example multiline comment"""
+#pip install pyjokes
